09825d1ab5835d9f1e01ea93fd9664c12098216b
憶初陽的那缕光 <1436159527@qq.com>
2024-03-27 20:43:44 +0800

    feat: 添加文档依赖

--- learnhub-user/src/main/java/com/github/learnhub/user/controller/UserController.java
+++ learnhub-user/src/main/java/com/github/learnhub/user/controller/UserController.java
@@ -1,6 +1,8 @@
 package com.github.learnhub.user.controller;
 
 import com.github.learnhub.user.service.IUserService;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.AllArgsConstructor;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -14,9 +16,11 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("user")
 @AllArgsConstructor
+@Tag(name = "用户模块")
 public class UserController {
     private final IUserService userService;
 
+    @Operation(summary = "hello", description = "hello1")
     @GetMapping("/hello")
     public String hello() {
         return userService.hello();
